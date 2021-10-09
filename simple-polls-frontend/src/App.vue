9bbebbc5fd8dfc3462d8b33e443f06f2e01945e6
Ondřej <ondra.saba@gmail.com>
2021-10-09 11:47:50 +0200

    created: base for frontend

---
+++ simple-polls-frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Inter, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
